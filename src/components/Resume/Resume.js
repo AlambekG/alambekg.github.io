980f65e00f950625d666e93a961221c10f26b914
Alambek Gulamidinov <77918145+AlambekG@users.noreply.github.com>
2024-08-06 06:08:55 +0000

    update cv

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Alambek_CV2407.pdf";
+import pdf from "../../Assets/../Assets/Alambek_CV2408.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
