cd501dc53950310b092c0fd205d236757c3cc1ef
Alambek Gulamidinov <77918145+AlambekG@users.noreply.github.com>
2025-09-08 16:39:51 +0900

    change content

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -14,32 +14,38 @@ function AboutCard() {
               <div className="aboutDiv">
               <span className="large"> Ulsan National Institute of Science and Technology (UNIST) </span>
               <br/>
-              <b className="purple medium"> BS in Computer Science and Engineering (2019-2024)</b> 
+              <b className="purple medium"> BS in Computer Science and Engineering (2019-2023)</b> 
               </div>
             </div>
             <br />
             <div>
               Experience:
               <div className="aboutDiv"> 
-                <span className="large"> Software Engineer (Seoul, 2024 March - Present) </span>
+                <span className="large"> Research Software Engineer (Seoul, 2025 July - present) </span>
                 <br/>
-                <span className="medium"> <a href="https://www.nepla.net/" className="purple">  Nepla Co. </a> 
-                , Early stage legal-tech startup </span>
+                <span className="medium"> <a href="https://www.stonelab.kr/" className="purple">  Stonelab inc. </a> 
+                , AI healthcare startup </span>
               </div>
               <div className="aboutDiv"> 
-                <span className="large"> R&D Software Engineer Intern (Ulsan, 2023 July - 2023 September) </span>
+                <span className="large"> Software Engineer (Seoul, 2024 March - 2025 June) </span>
+                <br/>
+                <span className="medium"> <a href="https://www.nepla.net/" className="purple">  Nepla Co. </a> 
+                , Legal-tech startup </span>
+              </div>
+              <div className="aboutDiv"> 
+                <span className="large"> Researche Software Engineer, Intern (Ulsan, 2023 July - 2023 September) </span>
                 <br/>
                 <span className="medium"> <a href="https://www.ibs.re.kr/eng.do" className="purple"> Institute for Basic Science </a> 
                 , basic science research institute </span>
               </div>
               <div className="aboutDiv"> 
-                <span className="large"> R&D Software Engineer Intern (Ulsan, 2023 March - 2023 June) </span>
+                <span className="large"> R&D Software Engineer, Intern (Ulsan, 2023 March - 2023 June) </span>
                 <br/>
                 <span className="medium"> <a href="https://pal.unist.ac.kr/" className="purple"> Pal lab </a> 
                 , Research laboratory at UNIST</span>
               </div>
               <div className="aboutDiv"> 
-                <span className="large"> Software Developer Intern (Seoul, 2022 December - 2023 February)</span>
+                <span className="large"> Software Engineer, Intern (Seoul, 2022 December - 2023 February)</span>
                 <br/>
                 <span className="medium"> <a href="https://o-n2.com/" className="purple"> OnSquare </a> 
                 , Tech startup with product downloaded 30+ million times on Google Play.</span>
